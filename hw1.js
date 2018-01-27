2681d09e2d879e1a41998db373947b1a4a7da23e
Jessica Yang <jessica.yang@cca.edu>
2018-01-26 16:40:52 -0800

    Create hw1.js

---
+++ hw1.js
@@ -0,0 +1,9 @@
+function repeatLine(n) {
+	var count = 0;
+	while (count < 10) {
+  line(random(width), random(height), random(width), random(height));
+  count = count + 1;
+	}
+}
+
+repeatLine(10);
